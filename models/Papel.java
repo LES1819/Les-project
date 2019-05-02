d22b0a8ea38396c5b3183b9e6514e0e2a080122c
LES1819 <49023559+LES1819@users.noreply.github.com>
2019-05-02 09:49:09 +0100

    Add files via upload

---
+++ models/Papel.java
@@ -0,0 +1,148 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpa.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author tiago
+ */
+@Entity
+@Table(name = "Papel")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Papel.findAll", query = "SELECT p FROM Papel p"),
+    @NamedQuery(name = "Papel.notAssociated", query = "SELECT p FROM Papel p WHERE p NOT IN(SELECT a.papel FROM PapelhasAtividade a)"),
+    @NamedQuery(name = "Papel.findByIdPapel", query = "SELECT p FROM Papel p WHERE p.idPapel = :idPapel"),
+    @NamedQuery(name = "Papel.findByNome", query = "SELECT p FROM Papel p WHERE p.nome = :nome"),
+    @NamedQuery(name = "Papel.findByDescricao", query = "SELECT p FROM Papel p WHERE p.descricao = :descricao"),
+    @NamedQuery(name = "Papel.findByDataCriacao", query = "SELECT p FROM Papel p WHERE p.dataCriacao = :dataCriacao")})
+public class Papel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idPapel")
+    private Integer idPapel;
+    @Size(max = 45)
+    @Column(name = "nome")
+    private String nome;
+    @Size(max = 500)
+    @Column(name = "descricao")
+    private String descricao;
+    @Column(name = "dataCriacao")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCriacao;
+    @JoinColumn(name = "Utilizador_idUtilizador", referencedColumnName = "idUtilizador")
+    @ManyToOne(optional = false)
+    private Utilizador utilizadoridUtilizador;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "papel")
+    private Collection<PapelhasAtividade> papelhasAtividadeCollection;
+
+    public Papel() {
+    }
+
+    public Papel(Integer idPapel) {
+        this.idPapel = idPapel;
+    }
+
+    public Integer getIdPapel() {
+        return idPapel;
+    }
+
+    public void setIdPapel(Integer idPapel) {
+        this.idPapel = idPapel;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Utilizador getUtilizadoridUtilizador() {
+        return utilizadoridUtilizador;
+    }
+
+    public void setUtilizadoridUtilizador(Utilizador utilizadoridUtilizador) {
+        this.utilizadoridUtilizador = utilizadoridUtilizador;
+    }
+
+    @XmlTransient
+    public Collection<PapelhasAtividade> getPapelhasAtividadeCollection() {
+        return papelhasAtividadeCollection;
+    }
+
+    public void setPapelhasAtividadeCollection(Collection<PapelhasAtividade> papelhasAtividadeCollection) {
+        this.papelhasAtividadeCollection = papelhasAtividadeCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idPapel != null ? idPapel.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Papel)) {
+            return false;
+        }
+        Papel other = (Papel) object;
+        if ((this.idPapel == null && other.idPapel != null) || (this.idPapel != null && !this.idPapel.equals(other.idPapel))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "jpa.entities.Papel[ idPapel=" + idPapel + " ]";
+    }
+    
+}
---
+++ models/Produto.java
@@ -0,0 +1,168 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpa.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author tiago
+ */
+@Entity
+@Table(name = "Produto")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Produto.findAll", query = "SELECT p FROM Produto p"),
+    @NamedQuery(name = "Produto.notAssociated", query = "SELECT p FROM Produto p WHERE p NOT IN(SELECT a.produto FROM ProdutohasAtividade a)"),
+    @NamedQuery(name = "Produto.findByIdProduto", query = "SELECT p FROM Produto p WHERE p.idProduto = :idProduto"),
+    @NamedQuery(name = "Produto.findByNome", query = "SELECT p FROM Produto p WHERE p.nome = :nome"),
+    @NamedQuery(name = "Produto.findByDescricao", query = "SELECT p FROM Produto p WHERE p.descricao = :descricao"),
+    @NamedQuery(name = "Produto.findByTipo", query = "SELECT p FROM Produto p WHERE p.tipo = :tipo"),
+    @NamedQuery(name = "Produto.findByDataCriacao", query = "SELECT p FROM Produto p WHERE p.dataCriacao = :dataCriacao")})
+public class Produto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idProduto")
+    private Integer idProduto;
+    @Size(max = 45)
+    @Column(name = "nome")
+    private String nome;
+    @Size(max = 500)
+    @Column(name = "descricao")
+    private String descricao;
+    @Size(max = 8)
+    @Column(name = "Tipo")
+    private String tipo;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "dataCriacao")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCriacao;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "produto")
+    private Collection<ProdutohasAtividade> produtohasAtividadeCollection;
+    @JoinColumn(name = "Utilizador_idUtilizador", referencedColumnName = "idUtilizador")
+    @ManyToOne(optional = false)
+    private Utilizador utilizadoridUtilizador;
+
+    public Produto() {
+    }
+
+    public Produto(Integer idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public Produto(Integer idProduto, Date dataCriacao) {
+        this.idProduto = idProduto;
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Integer getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Integer idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    @XmlTransient
+    public Collection<ProdutohasAtividade> getProdutohasAtividadeCollection() {
+        return produtohasAtividadeCollection;
+    }
+
+    public void setProdutohasAtividadeCollection(Collection<ProdutohasAtividade> produtohasAtividadeCollection) {
+        this.produtohasAtividadeCollection = produtohasAtividadeCollection;
+    }
+
+    public Utilizador getUtilizadoridUtilizador() {
+        return utilizadoridUtilizador;
+    }
+
+    public void setUtilizadoridUtilizador(Utilizador utilizadoridUtilizador) {
+        this.utilizadoridUtilizador = utilizadoridUtilizador;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idProduto != null ? idProduto.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Produto)) {
+            return false;
+        }
+        Produto other = (Produto) object;
+        if ((this.idProduto == null && other.idProduto != null) || (this.idProduto != null && !this.idProduto.equals(other.idProduto))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "jpa.entities.Produto[ idProduto=" + idProduto + " ]";
+    }
+    
+}
