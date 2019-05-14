7e3fd0c1f1588ee44d7a219ec5ba20c403cb8511
LES1819 <49023559+LES1819@users.noreply.github.com>
2019-05-14 13:25:43 +0100

    Update Atividade.java

--- models/Atividade.java
+++ models/Atividade.java
@@ -38,7 +38,7 @@ import javax.xml.bind.annotation.XmlTransient;
 @NamedQueries({
     @NamedQuery(name = "Atividade.findAll", query = "SELECT a FROM Atividade a"),
     @NamedQuery(name = "Atividade.findByIdAtividades", query = "SELECT a FROM Atividade a WHERE a.idAtividades = :idAtividades"),
-    @NamedQuery(name = "Atividade.findOriginal",query = "SELECT a FROM Atividade a WHERE a.processoidProcesso IS NULL"),
+    @NamedQuery(name = "Atividade.findOriginal", query = "SELECT a FROM Atividade a WHERE a.processoidProcesso IS NULL AND a.idAtividades NOT IN(SElECT b.idAtivi,deOriginal.idAtividades FROM Atividade b WHERE b.processoidProcesso.idProcesso = :param)"),
     @NamedQuery(name = "Atividade.isCopy",query ="SELECT a FROM Atividade a WHERE a.idAtividadeOriginal IS NOT NULL AND a.idAtividades = :atividade"),
     @NamedQuery(name = "Atividade.getCopies",query ="SELECT a FROM Atividade a WHERE a.idAtividadeOriginal = :atividade"),
     @NamedQuery(name = "Atividade.destroyAssociatedPapers", query ="DELETE FROM PapelhasAtividade p WHERE p.atividade = :atividade"),
