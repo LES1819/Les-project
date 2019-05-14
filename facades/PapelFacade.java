aeaa277d5c07739c72462b61a6e59ac3b2ea604f
LES1819 <49023559+LES1819@users.noreply.github.com>
2019-05-14 13:22:27 +0100

    Add files via upload

---
+++ facades/PapelFacade.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpa.session;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import jpa.entities.Papel;
+
+/**
+ *
+ * @author samuel
+ */
+@Stateless
+public class PapelFacade extends AbstractFacade<Papel> {
+
+    @PersistenceContext(unitName = "LES_Project_v2PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public PapelFacade() {
+        super(Papel.class);
+    }
+    
+    public void destroyPapel(Papel papel){
+        em.createNamedQuery("Papel.destroyAssociatedAtividade").setParameter("papel", papel);
+}
+    
+}
